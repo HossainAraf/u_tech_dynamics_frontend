36e2478ec193b28dc718b43889eebabb4e08f63f
Hossain <arafat.kd99@gmail.com>
2024-11-17 13:08:40 +0600

    Import & Use browser router, routes & route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header text-orange-900 text-center">
-        Hello, World!
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={< />} />
+      </Routes>
+    </Router>
   );
 }
 
