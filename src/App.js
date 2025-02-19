b81905bd12576440c879c63770f0072a23f8ff1b
Hossain <arafat.kd99@gmail.com>
2025-02-20 00:46:35 +0600

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
